4eae2ac52191bca140e3497ff87e784144feff58
cgajean@student.42.fr <cgajean@f6r9s11.clusters.42paris.fr>
2025-11-14 18:28:18 +0100

    Aggiungiamento delle funzione strrcmp e strrncmp

---
+++ libft/source/ft_strrcmp.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strrcmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cgajean <cgajean@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/23 14:09:26 by fox               #+#    #+#             */
+/*   Updated: 2025/11/14 18:18:07 by cgajean          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_strrcmp(const char *s1, const char *s2)
+{
+	size_t	pos_s1;
+	size_t	pos_s2;
+
+	pos_s1 = ft_strlen(s1) - 1;
+	pos_s2 = ft_strlen(s2) - 1;
+	while (pos_s1 >= 0 && pos_s2 >= 0)
+	{
+		if (s1[pos_s1] != s2[pos_s2])
+			return (s1[pos_s1] - s2[pos_s2]);
+		pos_s1--;
+		pos_s2--;
+	}
+	return (0);
+}
